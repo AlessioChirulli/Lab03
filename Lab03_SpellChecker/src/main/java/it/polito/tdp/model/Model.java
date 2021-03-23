b5642e1cde96eeed0a3a108b24854e1d4d9a532b
chiru <chiru@LAPTOP-1QRSM3OH.homenet.telecomitalia.it>
2021-03-23 12:57:10 +0100

    Errore nella ricerca dicotomica

---
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/model/Model.java
@@ -0,0 +1,81 @@
+package it.polito.tdp.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+
+	ArrayList<String> dizionario=new ArrayList<String>();
+	
+	public void loadDictionary(String language) {
+		dizionario.clear();
+	try {
+		FileReader fr=new FileReader("src/main/resources/"+language+".txt");
+		BufferedReader br= new BufferedReader(fr);
+		String word;
+		while((word=br.readLine())!=null) {
+			dizionario.add(word);
+		}
+		br.close();
+	}catch(IOException e) {
+		System.out.println("Errore nella lettura da file");
+	}	
+	}
+	
+	public ArrayList<String> spellCheckText(List<String> inputTextList){
+		ArrayList<String> paroleSbagliate=new ArrayList<String>();
+		for(String word:inputTextList) {
+			if(!dizionario.contains(word)) {
+			paroleSbagliate.add(word);	
+			}
+		}
+		return paroleSbagliate;
+	}
+	
+	public ArrayList<String> spellCheckTextLineare(List<String> inputTextList){
+		ArrayList<String> paroleSbagliate=new ArrayList<String>();
+		boolean controllo=false;
+		for(String word:inputTextList) {
+			controllo=false;
+			for(String s:dizionario) {
+			if(s.equals(word)) {
+				controllo=true;
+			}
+			}
+			if(!controllo)
+			paroleSbagliate.add(word);
+			}
+		return paroleSbagliate;
+		}
+		
+	
+	public ArrayList<String> spellCheckTextDicotomica(List<String> inputTextList){
+		ArrayList<String> paroleSbagliate=new ArrayList<String>();
+		int indice=dizionario.size()/2;
+		int max=dizionario.size();
+		int min=0;
+		boolean controllo=false;
+		for(String word:inputTextList) {
+			controllo=false;
+			while(min!=max){
+			if(word.compareTo(dizionario.get(indice))==0) {
+				controllo=true;
+				}else if(word.compareTo(dizionario.get(indice))>0) {
+					min=indice;
+					indice=(min+max)/2;
+				}else {
+					max=indice;
+					indice=(min+max)/2;
+				}
+			}
+			if(!controllo)
+			paroleSbagliate.add(word);
+		}
+		return paroleSbagliate;
+	}
+}
+	
+
--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,23 +1,98 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
+import it.polito.tdp.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    
+	private Model model;
+	
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ComboBox<String> comboLingue;
+
+    @FXML
+    private TextArea txtLingua;
+
+    @FXML
+    private Button btnCheck;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Label txtErrori;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Label txtTempo;
+
+    @FXML
+    void doCheck(ActionEvent event) {
+    double tempo=(double)System.nanoTime();
+    String testo=txtLingua.getText();
+    testo=testo.toLowerCase();
+    testo=testo.replaceAll("[.,\\/#!$%\\^&\\*;:{}=-_'()\\[\\]\"]", "");
+    StringTokenizer parole = new StringTokenizer(testo, " ");
+    ArrayList<String> listaParole=new ArrayList<String>();
+    while(parole.hasMoreTokens())  {
+    	listaParole.add(parole.nextToken());
+    }
+    this.model.loadDictionary((String)comboLingue.getValue());
+    ArrayList<String>paroleSbagliate=this.model.spellCheckTextDicotomica(listaParole);
+    String s="";
+    for(String parola:paroleSbagliate) {
+    	s+=parola+"\n";
+    }
+    double tempo2=(double)System.nanoTime();
+    txtErrori.setText("The text contains "+paroleSbagliate.size()+" errors");
+    txtResult.setText(s);
+    txtTempo.setText("Spell check completed in "+((tempo2-tempo)/1e9)+" secondi");
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    txtResult.clear();
+    txtTempo.setText("");
+    txtLingua.clear();
+    comboLingue.setValue("");
+    txtErrori.setText("");
+    }
+
+    public void setModel(Model m) {
+    	this.model = m ;
+    	String language[]=new String[]{"Italian","English"};
+    	comboLingue.getItems().addAll(language);
+    	
+    	
+    }
+    
+    @FXML
     void initialize() {
+        assert comboLingue != null : "fx:id=\"comboLingue\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtLingua != null : "fx:id=\"txtLingua\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtErrori != null : "fx:id=\"txtErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTempo != null : "fx:id=\"txtTempo\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
